702d2a6f73b61e5870459094fe7dce3760fe41f1
bblais23 <ben.blais6@gmail.com>
2019-10-17 18:45:27 -0400

    Configure eleventy

---
+++ .eleventy.js
@@ -0,0 +1,27 @@
+/**
+ * Copyright 2019 Benjamin Blais
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ */
+module.exports = {
+	dir: {
+		input: 'src',
+		output: '_site'
+	},
+	templateFormats: [
+		"md",
+		"html",
+		"css",
+		"jpg",
+		"png",
+		"gif",
+		"ico",
+		"svg",
+		"njk",
+		"ttf",
+		"otf",
+		"woff",
+		"woff2",
+	],
+	passthroughFileCopy: true
+}
