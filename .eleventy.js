62a77f3ea6e6935e21fe82abf06e2ee1a5950f21
bblais23 <ben.blais6@gmail.com>
2019-10-18 14:33:07 -0400

    Syntax highlighting plugin

--- .eleventy.js
+++ .eleventy.js
@@ -3,25 +3,32 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  */
-module.exports = {
-	dir: {
-		input: 'src',
-		output: '_site'
-	},
-	templateFormats: [
-		"md",
-		"html",
-		"css",
-		"jpg",
-		"png",
-		"gif",
-		"ico",
-		"svg",
-		"njk",
-		"ttf",
-		"otf",
-		"woff",
-		"woff2",
-	],
-	passthroughFileCopy: true
+
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+module.exports = (eleventyConfig) => {
+
+	eleventyConfig.addPlugin(syntaxHighlight);
+
+	return {
+		dir: {
+			input: 'src',
+			output: '_site'
+		},
+		templateFormats: [
+			"md",
+			"html",
+			"css",
+			"jpg",
+			"png",
+			"gif",
+			"ico",
+			"svg",
+			"njk",
+			"ttf",
+			"otf",
+			"woff",
+			"woff2",
+		],
+		passthroughFileCopy: true
+	}
 }
